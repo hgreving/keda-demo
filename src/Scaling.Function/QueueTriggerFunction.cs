53ca10d83d793b811e8954fa4017b6ec80634b39
Harmjan Greving <Harmjan.Greving@iddinkgroup.com>
2020-10-29 12:03:10 +0100

    Updated image

--- src/Scaling.Function/QueueTriggerFunction.cs
+++ src/Scaling.Function/QueueTriggerFunction.cs
@@ -13,7 +13,7 @@ namespace Scaling.QueueTriggers.Function
         {
             var item = JsonConvert.DeserializeObject<QueueItem>(myQueueItem);
             Thread.Sleep(item.Duration); // Do something that takes a long time...
-            log.LogInformation($"C# Queue trigger function processed: {item.Message} with a duration of {item.Duration} ms.");
+            log.LogInformation($"C# Queue trigger function processed message: {item.Message} with a duration of {item.Duration}ms.");
         }
     }
 }
